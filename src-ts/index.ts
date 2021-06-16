5f14ab60f6f9e0b94a764111cf83811e0c2c1f4e
chinsun9 <chin_sung@naver.com>
2021-06-16 17:59:50 +0900

    feat: roll animation

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,1 +1,24 @@
 console.log(`hello random dice`);
+
+function getRandom(min: number, max: number) {
+  const newmin = Math.ceil(min);
+  const newmax = Math.floor(max);
+  return Math.floor(Math.random() * (newmax - newmin)) + min;
+}
+
+console.log(getRandom(1, 6));
+
+const bodyElement = document.body;
+const cubeContainerElement = document.querySelector<HTMLDivElement>(
+  'body > div > div.cubeContainer'
+)!;
+
+bodyElement.addEventListener('click', (event) => {
+  console.log(event);
+
+  const xrand = getRandom(0, 360);
+  const yrand = getRandom(0, 360);
+  const zrand = getRandom(0, 360);
+
+  cubeContainerElement.style.transform = `rotateX(${xrand}deg) rotateY(${yrand}deg) rotateZ(${zrand}deg)`;
+});
