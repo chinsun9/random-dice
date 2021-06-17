ecada323be72d2d1ca4f2f0fa5807af31ef3ca7b
chinsun9 <chin_sung@naver.com>
2021-06-17 09:40:14 +0900

    feat: roll dice
    여러번 회전하기 이후 결과값으로 도배

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -12,13 +12,51 @@ const bodyElement = document.body;
 const cubeContainerElement = document.querySelector<HTMLDivElement>(
   'body > div > div.cubeContainer'
 )!;
+const cubeElement = cubeContainerElement.firstElementChild;
 
-bodyElement.addEventListener('click', (event) => {
-  console.log(event);
-
+async function rollDice() {
   const xrand = getRandom(0, 360);
-  const yrand = getRandom(0, 360);
   const zrand = getRandom(0, 360);
+  const yrand = getRandom(0, 360);
 
   cubeContainerElement.style.transform = `rotateX(${xrand}deg) rotateY(${yrand}deg) rotateZ(${zrand}deg)`;
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 300);
+  });
+}
+
+function faceDice() {
+  // change number all side
+  const randomResult = getRandom(1, 7);
+
+  const array = cubeElement?.children!;
+
+  for (let index = 0; index < array.length; index += 1) {
+    const element = array[index];
+    element.innerHTML = randomResult.toString();
+  }
+}
+
+function initDice() {
+  const array = cubeElement?.children!;
+
+  for (let index = 0; index < array.length; index += 1) {
+    const element = array[index];
+    element.innerHTML = (index + 1).toString();
+  }
+}
+
+bodyElement.addEventListener('click', async (event) => {
+  console.log(event);
+
+  initDice();
+
+  for (let index = 0; index < 5; index += 1) {
+    // eslint-disable-next-line no-await-in-loop
+    await rollDice();
+  }
+  faceDice();
+  rollDice();
 });
