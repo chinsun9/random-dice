be276ec4927d83c1652a339da35fb6ba5d3ef22d
chinsun9 <chin_sung@naver.com>
2021-06-18 17:58:01 +0900

    Updates

--- js/index.js
+++ js/index.js
@@ -7,8 +7,30 @@ bodyElement.addEventListener('click', function (event) {
     dice.initDice();
     dice.rollDice();
 });
-window.addEventListener('deviceorientation', function (event) {
+var lastTimestamp = new Date().getTime();
+var speedX = 0;
+var speedY = 0;
+var speedZ = 0;
+window.addEventListener('devicemotion', function (event) {
     var a = document.querySelector('body > div.footer');
-    a.innerHTML = JSON.stringify(event);
+    var currentTime = new Date().getTime();
+    if (lastTimestamp === undefined) {
+        lastTimestamp = new Date().getTime();
+        return; // ignore first call, we need a reference time
+    }
+    //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
+    if (!event.acceleration)
+        return;
+    speedX +=
+        ((event.acceleration.x / 1000) * ((currentTime - lastTimestamp) / 1000)) /
+            3600;
+    speedY +=
+        ((event.acceleration.y / 1000) * ((currentTime - lastTimestamp) / 1000)) /
+            3600;
+    speedZ +=
+        ((event.acceleration.z / 1000) * ((currentTime - lastTimestamp) / 1000)) /
+            3600;
+    lastTimestamp = currentTime;
+    a.innerHTML = JSON.stringify([speedX, speedY, speedZ]);
 });
 //# sourceMappingURL=index.js.map
