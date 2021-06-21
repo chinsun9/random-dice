dddc929eb7aa96c97cd3729b7488ce53c0db3748
chinsun9 <chin_sung@naver.com>
2021-06-21 17:29:49 +0900

    Updates

--- js/index.js
+++ js/index.js
@@ -4,33 +4,25 @@ var cubeContainerElement = document.querySelector('body > div > div.cubeContaine
 var dice = new Dice(cubeContainerElement);
 bodyElement.addEventListener('click', function (event) {
     event.preventDefault();
-    dice.initDice();
-    dice.rollDice();
+    dice.roll();
 });
 var lastTimestamp = new Date().getTime();
-var speedX = 0;
-var speedY = 0;
-var speedZ = 0;
 window.addEventListener('devicemotion', function (event) {
-    var a = document.querySelector('body > div.footer');
-    var currentTime = new Date().getTime();
-    if (lastTimestamp === undefined) {
-        lastTimestamp = new Date().getTime();
-        return; // ignore first call, we need a reference time
-    }
-    //  m/s² / 1000 * (miliseconds - miliseconds)/1000 /3600 => km/h (if I didn't made a mistake)
-    if (!event.acceleration)
+    event.preventDefault();
+    if (!event.acceleration) {
+        console.log("no acceleration");
         return;
-    speedX +=
-        ((event.acceleration.x / 1000) * ((currentTime - lastTimestamp) / 1000)) /
-            3600;
-    speedY +=
-        ((event.acceleration.y / 1000) * ((currentTime - lastTimestamp) / 1000)) /
-            3600;
-    speedZ +=
-        ((event.acceleration.z / 1000) * ((currentTime - lastTimestamp) / 1000)) /
-            3600;
+    }
+    var currentTime = new Date().getTime();
+    if (currentTime - lastTimestamp < 300) {
+        return;
+    }
     lastTimestamp = currentTime;
-    a.innerHTML = JSON.stringify([speedX, speedY, speedZ]);
+    var isActive = Math.max(Math.abs(event.acceleration.x), Math.abs(event.acceleration.y), Math.abs(event.acceleration.z));
+    if (isActive >= 4) {
+        console.log("run");
+        event.preventDefault();
+        dice.roll();
+    }
 });
 //# sourceMappingURL=index.js.map
