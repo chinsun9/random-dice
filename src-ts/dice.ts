e14e15604d350a9e2752223bc7bcf962744da05a
chinsun9 <chin_sung@naver.com>
2021-07-01 11:30:32 +0900

    fix: remove init dice rotate
    - initDice에서 회전값 0으로 초기화하는 코드 제거

--- src-ts/dice.ts
+++ src-ts/dice.ts
@@ -15,6 +15,8 @@ class Dice {
 
   sto: ReturnType<typeof setTimeout>;
 
+  isRolling: boolean;
+
   constructor(cubeContainerElement: HTMLDivElement) {
     this.x = 0;
     this.y = 0;
@@ -22,19 +24,14 @@ class Dice {
     this.cubeContainerElement = cubeContainerElement;
     this.cubeElement = cubeContainerElement.firstElementChild!;
     this.sto = setTimeout(() => {}, 0);
-  }
-
-  initXYZ() {
-    this.x = 0;
-    this.y = 0;
-    this.z = 0;
+    this.isRolling = false;
   }
 
   rollDice() {
     clearTimeout(this.sto);
     this.sto = setTimeout(() => {
       this.faceDice();
-    }, 1000);
+    }, 300);
     this.x += getRandom(base * 7, base * 10);
     this.y += getRandom(base, base * 2);
     this.z += getRandom(base, base * 2);
@@ -53,24 +50,29 @@ class Dice {
       const element = array[index];
       element.innerHTML = randomResult.toString();
     }
+
+    this.isRolling = false;
   }
 
   initDice() {
-    this.initXYZ();
     const array = this.cubeElement?.children!;
 
     for (let index = 0; index < array.length; index += 1) {
       const element = array[index];
       element.innerHTML = (index + 1).toString();
     }
+  }
 
-    this.cubeContainerElement.style.transition = 'none';
-    setTimeout(() => {
-      this.cubeContainerElement.style.transform = `rotateX(${0}deg) rotateY(${0}deg) rotateZ(${0}deg)`;
-      this.cubeContainerElement.style.transition = 'all 4s';
-      this.cubeContainerElement.style.transitionTimingFunction =
-        'transition-timing-function: cubic-bezier(0,.6,0,.98)';
-    }, 0);
+  async roll() {
+    if (this.isRolling) {
+      console.log(`already rolling`);
+      return;
+    }
+
+    this.isRolling = true;
+
+    this.initDice();
+    this.rollDice();
   }
 }
 
